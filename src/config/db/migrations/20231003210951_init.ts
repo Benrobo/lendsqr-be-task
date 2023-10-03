e8f2551950815c9df5f9b0bdb21ce935f5fae72b
Benrobo <alumonabenaiah71@gmail.com>
2023-10-04 00:21:33 +0100

    fix: fixed related foriegn key issues and connectionString

--- src/config/db/migrations/20231003210951_init.ts
+++ src/config/db/migrations/20231003210951_init.ts
@@ -9,7 +9,7 @@ export async function up(knex: Knex): Promise<void> {
       table.string("transaction_pin");
       table.string("password_hash");
       table.string("refresh_token");
-      table.dateTime("createdAt").defaultTo(knex.fn.now());
+      table.timestamps(true, true);
 
       table.primary(["id"]);
       table.unique(["id", "email", "username"]);
@@ -17,50 +17,34 @@ export async function up(knex: Knex): Promise<void> {
     .createTable("wallet", (table) => {
       table.string("id").notNullable();
       table.string("user_id");
-      table.decimal("balance", 0, 10);
+      table.decimal("balance", 10);
       table.string("currency");
-      table.dateTime("createdAt").defaultTo(knex.fn.now());
+      table.timestamps(true, true);
 
       table.primary(["id"]);
       table.unique(["user_id"]);
-      table
-        .foreign("user_id")
-        .references("users.id")
-        .deferrable("deferred")
-        .onDelete("CASCADE");
+      table.foreign("user_id").references("users.id").onDelete("CASCADE");
     })
     .createTable("transactions", (table) => {
       table.string("id").notNullable();
       table.string("sender_id");
       table.string("receiver_id");
-      table.decimal("amount", 0, 10);
+      table.decimal("amount", 10);
       table.string("status");
-      table.decimal("fee", 0, 10);
-      table.dateTime("createdAt").defaultTo(knex.fn.now());
+      table.decimal("fee", 10);
+      table.timestamps(true, true);
 
       table.primary(["id"]);
-      table
-        .foreign("sender_id")
-        .references("users.id")
-        .deferrable("deferred")
-        .onDelete("CASCADE");
-      table
-        .foreign("sender_id")
-        .references("users.id")
-        .deferrable("deferred")
-        .onDelete("CASCADE");
+      table.foreign("sender_id").references("users.id").onDelete("CASCADE");
+      table.foreign("receiver_id").references("users.id").onDelete("CASCADE");
     })
     .createTable("payment_events", (table) => {
       table.string("id").notNullable();
       table.string("user_id");
-      table.dateTime("createdAt").defaultTo(knex.fn.now());
+      table.timestamps(true, true);
 
       table.primary(["id"]);
-      table
-        .foreign("user_id")
-        .references("users.id")
-        .deferrable("deferred")
-        .onDelete("CASCADE");
+      table.foreign("user_id").references("users.id").onDelete("CASCADE");
     });
 }
 
