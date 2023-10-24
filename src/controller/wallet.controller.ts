ab859430e7b454cb7bb63db30c2f7b8f6d3e971a
Benrobo <alumonabenaiah71@gmail.com>
2023-10-24 19:06:03 +0100

    feat: added funding of wallet

--- src/controller/wallet.controller.ts
+++ src/controller/wallet.controller.ts
@@ -1,11 +1,58 @@
 import { Request, Response } from "express";
-import { RESPONSE_CODE } from "../@types";
+import { FundWalletType, RESPONSE_CODE, TransactionType } from "../@types";
 import BaseController from "./base.controller";
+import db from "../config/db";
+import bcrypt from "bcryptjs";
+import shortUUID from "short-uuid";
 
 export default class WalletController extends BaseController {
   constructor() {
     super();
   }
 
-  async fund(req: Request, res: Response) {}
+  async fund(req: Request, res: Response) {
+    const userId = (req as any)?.user?.id;
+    const { pin, amount } = req.body as FundWalletType;
+
+    // check if pin is valid
+    const details = await db("users")
+      .where("users.id", userId)
+      .join("wallet", "wallet.user_id", "=", "users.id")
+      .select("users.transaction_pin", "wallet.balance");
+
+    const { transaction_pin, balance } = details[0];
+    if (!bcrypt.compareSync(pin, transaction_pin)) {
+      return this.error(
+        res,
+        RESPONSE_CODE.INVALID_TRANSACTION_PIN,
+        "Invalid transaction credentials",
+        400
+      );
+    }
+
+    // fund wallet
+    const totalBalance = balance + amount;
+    await db("wallet")
+      .update({
+        balance: totalBalance,
+      })
+      .where("wallet.user_id", userId);
+
+    // create transaction
+    await db("transactions").insert({
+      id: shortUUID.generate(),
+      receiver_id: userId,
+      amount,
+      status: TransactionType[TransactionType.success],
+      fee: 0,
+    });
+
+    this.success(
+      res,
+      RESPONSE_CODE.WALLET_FUNDED_SUCCESSFULL,
+      "wallet funded successfully.",
+      201,
+      { balance: totalBalance }
+    );
+  }
 }
--- src/routes/wallet.route.ts
+++ src/routes/wallet.route.ts
@@ -2,6 +2,8 @@ import express from "express";
 import useCatchErrors from "../error/catchErrors";
 import WalletController from "../controller/wallet.controller";
 import { isAuthenticated } from "../middlewares/auth";
+import zodValidation from "../middlewares/zodValidation";
+import { WalletFundingSchema } from "../helper/validate";
 
 export default class WalletRoute {
   router = express.Router();
@@ -16,6 +18,7 @@ export default class WalletRoute {
     this.router.post(
       `${this.path}`,
       isAuthenticated,
+      zodValidation(WalletFundingSchema),
       useCatchErrors(this.walletController.fund.bind(this.walletController))
     );
   }
