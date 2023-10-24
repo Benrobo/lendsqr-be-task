aa714c9801a2052cb42f4b5f44e1f7e36ca85492
Benrobo <alumonabenaiah71@gmail.com>
2023-10-24 10:06:49 +0100

    feat: added jwt helper class

--- src/helper/jwt.ts
+++ src/helper/jwt.ts
@@ -1,1 +1,22 @@
-class JWT {}
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export default class JWT {
+  private _secret = process.env.JWT_SECRET;
+
+  async accessToken(data: any) {
+    const token = await jwt.sign({ data }, this._secret, { expiresIn: "1h" });
+    return token;
+  }
+
+  async refreshToken(data: any) {
+    const token = await jwt.sign({ data }, this._secret, { expiresIn: "365d" });
+    return token;
+  }
+
+  async verifyToken(token: any) {
+    const decoded = await jwt.verify(token, this._secret);
+    return decoded;
+  }
+}
