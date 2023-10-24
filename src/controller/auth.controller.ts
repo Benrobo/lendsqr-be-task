a7807a290ea29e551c5c9e3b0313b5b0144b43b0
Benrobo <alumonabenaiah71@gmail.com>
2023-10-24 10:06:57 +0100

    feat: added user login

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import BaseController from "./base.controller";
-import { SignupSchema } from "../helper/validate";
+import { LoginSchema, SignupSchema } from "../helper/validate";
 import { RESPONSE_CODE } from "../@types/index";
 import db from "../config/db";
 import bcrypt from "bcryptjs";
 import shortUUID from "short-uuid";
+import JWT from "../helper/jwt";
 
 export default class AuthController extends BaseController {
   constructor() {
@@ -35,6 +36,7 @@ export default class AuthController extends BaseController {
       );
     }
 
+    // generate password hash
     const hashPwd = bcrypt.hashSync(password, 10);
 
     // save user record
@@ -62,5 +64,65 @@ export default class AuthController extends BaseController {
     );
   }
 
-  async login(req: Request, res: Response) {}
+  async login(req: Request, res: Response) {
+    const payload = req.body;
+    const validate = LoginSchema.safeParse(payload);
+    if (!validate.success) {
+      return this.error(
+        res,
+        RESPONSE_CODE.INVALID_FIELDS,
+        "One or more of the input is invalid.",
+        400
+      );
+    }
+    const { email, password } = validate.data;
+
+    // check if user exists
+    const users = await db("users").where("email", email);
+    const user = users[0];
+    if (!user) {
+      return this.error(
+        res,
+        RESPONSE_CODE.USER_NOT_FOUND,
+        "account notfound",
+        404
+      );
+    }
+
+    // check password match
+    const pwdHash = user.password_hash;
+    const userId = user.id;
+
+    // compare password
+    if (!bcrypt.compareSync(password, pwdHash)) {
+      return this.error(
+        res,
+        RESPONSE_CODE.INVALID_LOGIN_CREDENTIALS,
+        "Invalid login credentials",
+        400
+      );
+    }
+
+    // generate access and refresh token token
+    const jwt = new JWT();
+    const refreshToken = await jwt.refreshToken(userId);
+    const accessToken = await jwt.refreshToken(userId);
+
+    await db("users")
+      .update({
+        refresh_token: refreshToken,
+      })
+      .where("email", email);
+
+    this.success(
+      res,
+      RESPONSE_CODE.LOGIN_SUCCESSFULL,
+      "user logged in successfully",
+      201,
+      {
+        accessToken,
+        userId,
+      }
+    );
+  }
 }
