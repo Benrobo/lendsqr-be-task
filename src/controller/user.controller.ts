ac258026ff71737a3464e672d90f7ea30824585d
Benrobo <alumonabenaiah71@gmail.com>
2023-10-24 18:40:30 +0100

    feat: added transaction pin creation

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,10 +1,34 @@
+import { Request, Response } from "express";
 import { RESPONSE_CODE } from "../@types";
 import BaseController from "./base.controller";
+import { TransactionPinSchema } from "../helper/validate";
+import bcrypt from "bcryptjs";
+import db from "../config/db";
 
 export default class UserController extends BaseController {
   constructor() {
     super();
   }
 
-  async getUser(req, res) {}
+  async getInfo(req, res) {}
+
+  async updateTransactionPin(req: Request, res: Response) {
+    const userId = (req as any).user?.id;
+    const { pin } = req.body;
+    const pinHash = bcrypt.hashSync(pin.toString(), 10);
+
+    // update transaction
+    await db("users")
+      .update({
+        transaction_pin: pinHash,
+      })
+      .where("id", userId);
+
+    this.success(
+      res,
+      RESPONSE_CODE.TRANSACTION_PIN_UPDATED_SUCCESSFULL,
+      "Transaction pin updated successfully",
+      201
+    );
+  }
 }
