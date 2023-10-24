63494a3c2f69fb1bcae6cc6d9f2cfa9a00887dd8
Benrobo <alumonabenaiah71@gmail.com>
2023-10-24 18:32:35 +0100

    feat: added zod validation middleware

---
+++ src/middlewares/zodValidation.ts
@@ -0,0 +1,20 @@
+import { RESPONSE_CODE } from "../@types";
+import { Request, Response, NextFunction } from "express";
+import { AnyZodObject } from "zod";
+
+export default function zodValidation(schema: AnyZodObject) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync(req.body ?? req.params ?? req.query);
+      return next();
+    } catch (error) {
+      return res.status(400).json({
+        code: RESPONSE_CODE[RESPONSE_CODE.VALIDATION_ERROR],
+        error: {
+          message: error?.issues[0]?.message,
+          error,
+        },
+      });
+    }
+  };
+}
